58340b876ad79c88f6a3968b055482efc0fa4c8a
Anton Prilepsky <towa48@gmail.com>
2018-10-09 21:55:05 +0300

    Implement webdav root options;

--- cmd/go-libre-storage/webdav.go
+++ cmd/go-libre-storage/webdav.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func WebDav(r *gin.Engine) {
-	authorized := r.Group("/webdav", WebDavBasicAuth())
+const Prefix string = "/webdav"
 
-	authorized.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "WebDav")
+func WebDav(r *gin.Engine) {
+	authorized := r.Group(Prefix, WebDavBasicAuth())
+
+	authorized.OPTIONS("/*path", func(c *gin.Context) {
+		path := stripPrefix(c.Request.URL.Path)
+		if path == "/" {
+			c.Header("Allow", "OPTIONS, PROPFIND")
+		} else {
+			// TBD
+		}
+
+		c.Header("DAV", "1, 2")
+		c.Header("MS-Author-Via", "DAV")
 	})
+
+	authorized.Handle("GET", "/*path", func(c *gin.Context) {
+		c.Status(403)
+	})
+
+	authorized.Handle("PROPFIND", "/*path", func(c *gin.Context) {
+		path := stripPrefix(c.Request.URL.Path)
+		fmt.Println("Request: " + path)
+
+		c.Status(403)
+	})
+}
+
+func stripPrefix(path string) string {
+	if result := strings.TrimPrefix(path, Prefix); len(result) < len(path) {
+		return result
+	}
+
+	return path
 }
