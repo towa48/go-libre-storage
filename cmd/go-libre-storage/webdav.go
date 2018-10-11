b7e251e2ce87b32f04209df537f33c5b396348bf
Anton Prilepsky <towa48@gmail.com>
2018-10-11 21:08:11 +0300

    Implement stub for webdav deep 0 folder;

--- cmd/go-libre-storage/webdav.go
+++ cmd/go-libre-storage/webdav.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 const Prefix string = "/webdav"
+const XmlDocumentType string = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
 func WebDav(r *gin.Engine) {
 	authorized := r.Group(Prefix, WebDavBasicAuth())
@@ -30,8 +34,32 @@ func WebDav(r *gin.Engine) {
 
 	authorized.Handle("PROPFIND", "/*path", func(c *gin.Context) {
 		path := stripPrefix(c.Request.URL.Path)
-		fmt.Println("Request: " + path)
+		depth := parseDepth(c.Request.Header.Get("Depth"))
 
+		if depth == invalidDepth || depth == infiniteDepth {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		data, err := c.GetRawData()
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		var req Propfind
+		err = xml.Unmarshal(data, &req)
+		// TODO: analyze request payload
+
+		if path == "/" && depth == 0 {
+			resp := getMultistatusResponse("/") // TODO: get root folder properties
+
+			c.Writer.Write([]byte(XmlDocumentType))
+			c.XML(http.StatusOK, resp)
+			return
+		}
+
+		fmt.Println("Request: " + path)
 		c.Status(403)
 	})
 }
@@ -42,4 +70,75 @@ func stripPrefix(path string) string {
 	}
 
 	return path
+}
+
+const (
+	infiniteDepth = -1
+	invalidDepth  = -2
+)
+
+func parseDepth(s string) int {
+	switch s {
+	case "0":
+		return 0
+	case "1":
+		return 1
+	case "infinity":
+		return infiniteDepth
+	}
+	return invalidDepth
+}
+
+func getMultistatusResponse(url string) Multistatus {
+	timeNow := time.Now()
+	createdTimeVal := timeNow.Format(time.RFC3339)
+	modifiedTimeVal := timeNow.Format(time.RFC1123)
+
+	props := []PropStat{
+		{
+			Status:           "HTTP/1.1 200 OK",
+			CreationDate:     createdTimeVal,
+			DisplayName:      "box",
+			LastModifiedDate: modifiedTimeVal,
+			ResourceType:     &CollectionResourceType{},
+		},
+	}
+
+	return Multistatus{
+		XmlNs: "DAV:",
+		Reponse: MultistatusResponse{
+			Href:  "/",
+			Props: props,
+		},
+	}
+}
+
+type Propfind struct {
+	XmlName xml.Name `xml:"propfind"`
+	XmlNs   string   `xml:"xmlns,attr"`
+	Allprop string   `xml:"allprop"`
+}
+
+type Multistatus struct {
+	XMLName xml.Name            `xml:"d:multistatus"`
+	XmlNs   string              `xml:"xmlns:d,attr"`
+	Reponse MultistatusResponse `xml:"d:response"`
+}
+
+type MultistatusResponse struct {
+	Href  string     `xml:"d:href"`
+	Props []PropStat `xml:"d:propstat"`
+}
+
+type PropStat struct {
+	Status           string      `xml:"d:status"`
+	CreationDate     string      `xml:"d:prop>d:creationdate"`
+	DisplayName      string      `xml:"d:prop>d:displayname"`
+	LastModifiedDate string      `xml:"d:prop>d:getlastmodified"`
+	ResourceType     interface{} `xml:"d:prop>d:resourcetype"`
+}
+
+type CollectionResourceType struct {
+	XMLName   xml.Name `xml:"d:resourcetype"`
+	FakeValue string   `xml:"d:collection"`
 }
