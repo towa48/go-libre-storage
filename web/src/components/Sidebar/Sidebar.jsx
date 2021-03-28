d954e7859983946431aaad80fea2774ca4f16094
Anton Prilepsky <towa48@gmail.com>
2021-03-28 17:32:36 +0300

    Fix button align

--- web/src/components/Sidebar/Sidebar.jsx
+++ web/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import AddIcon from '@material-ui/icons/Add';
 import './Sidebar.scss';
 
 export function Sidebar() {
     return (
         <div class="sidebar sidebar_fixed">
             <div class="sidebar__buttons">
-                <Button variant="contained"color="primary">Upload</Button>
-                <Button variant="contained">Create</Button>
+                <Box
+                    sx={{
+                        '& button': {
+                        mb: 2,
+                        },
+                    }}>
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        startIcon={<CloudUploadIcon />}>Upload</Button>
+                    <Button
+                        variant="outlined"
+                        fullWidth
+                        startIcon={<AddIcon />}>Create</Button>
+                </Box>
             </div>
             <div class="navigation sidebar__navigation">
                 <div class="navigation__items">
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './containers/AppMain/App';
 import store from './containers/AppMain/store';
-import { Provider } from 'react-redux';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </StyledEngineProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('app')
